f25ea64616f581d3c759e3ffc68ab8dc369e0b6d
lilscan <info@lilscan.com>
2022-01-08 23:11:42 +0100

    disable dev tool

--- src/main.js
+++ src/main.js
@@ -65,7 +65,7 @@ const createWindow = () => {
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
